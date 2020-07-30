852d5b1a0181f3121677cdcadf951d71a7cb1a42
Geraldo Quende <adolfoquende@hotmail.com>
2020-07-30 18:43:47 +0100

    Aula 11 Criando um Dismissed

---
+++ learn/lib/TesteAprendizagem/Aula6.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:learn/models/item.dart';
+
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Adolfo Quende',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Homeage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+
+    items.add(Item(title: "Ola", done: true));
+    items.add(Item(title: "Agora Sim"), done: false);
+    items.add(Item(title: "Adolfo E mau", done: true));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ola"),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (context, index) {
+          return Text(widget.items[index].title);
+        },
+      ),
+    );
+  }
+}
